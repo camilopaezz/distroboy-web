a0273827454b43e4fbc1190f70f314ae578bb07b
Camilo Paez <camilopaez.dev@gmail.com>
2021-08-02 07:50:13 -0500

    feat: use a custom font

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,31 @@
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
 import Navbar from '../components/Navbar'
+
+import '@fontsource/inter/400.css'
+import '@fontsource/inter/700.css'
+import '@fontsource/inter/800.css'
+import '@fontsource/inter/900.css'
+
+const extendedTheme = extendTheme({
+  fonts: {
+    heading: 'Inter',
+    body: 'Inter'
+  }
+})
 
 function MyApp ({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={extendedTheme}>
       <ColorModeScript initialColorMode='dark' />
       <Navbar />
       <Component {...pageProps} />
+      <style jsx>
+        {`
+          body {
+            background-color: #1A202C;
+          }
+        `}
+      </style>
     </ChakraProvider>
   )
 }
